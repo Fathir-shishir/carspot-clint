36ea0f318fc75ae891866fae664391ba8448db62
Fathir Ahmed Shishir <mfa.shishir@gmail.com>
2022-05-08 18:17:15 +0200

    about section deleted

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Card } from 'react-bootstrap';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
 
@@ -6,7 +7,15 @@ const About = () => {
     const [user] = useAuthState(auth);
     return (
         <div>
-            <h1> {user?.displayName}</h1>
+            <Card className='mx-auto mt-5' style={{ width: '25%' }}>
+  <Card.Img variant="top" src={user?.photoURL} />
+  <Card.Body>
+    <Card.Title>{user?.displayName}</Card.Title>
+    <Card.Title>Email :{user?.email}</Card.Title>
+    
+    
+  </Card.Body>
+</Card>
         </div>
     );
 };
