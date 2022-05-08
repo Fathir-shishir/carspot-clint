b1b1cf176eec6f779345c46c7a44ff952124e424
Fathir Ahmed Shishir <mfa.shishir@gmail.com>
2022-05-08 17:22:44 +0200

    footer updated

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <div className='mt-5 bg-dark p-3'>
-            <p className='text-white text-center'>© All Rights Reserved by My Footer</p>
+            <p className='text-white text-center'> All Rights Reserved by © 2022</p>
         </div>
     );
 };
